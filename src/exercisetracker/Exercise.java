9c02a7777b01e5d6f6add9ec26f00d71eeac6281
Troy <troycarper06@gmail.com>
2026-04-15 13:12:22 -0500

    added getters and to stringto exercise.java

--- src/exercisetracker/Exercise.java
+++ src/exercisetracker/Exercise.java
@@ -1,8 +1,8 @@
 package exercisetracker;
-//
+
 enum TypeOfExercise{
     //squat,hinge,row,press;
-    //chest,back,leg,shoulders,arms
+    chest,back,leg,shoulders,arms
 }
 
 public class Exercise{
@@ -19,6 +19,36 @@ public class Exercise{
         this.sets = sets;
         this.rpe = rpe;
         this.exerciseType = exerciseType;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public int getRpe() {
+        return rpe;
+    }
+
+    public TypeOfExercise getExerciseType() {
+        return exerciseType;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder(exerciseName);
+        sb.append(reps);
+        sb.append(sets);
+        sb.append(rpe);
+        sb.append(exerciseType);
+        return sb.toString();
     }
 
     
